7547ffab4e0596e5c78d480c94eca8587822da4e
Crowdbotics <team@crowdbotics.com>
2020-12-04 06:01:25 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import GGHHH
+
+admin.site.register(GGHHH)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -22,6 +22,17 @@ class CustomText(models.Model):
     def field(self):
         return "title"
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
 
 class HomePage(models.Model):
     body = models.TextField()
@@ -33,3 +44,16 @@ class HomePage(models.Model):
     @property
     def field(self):
         return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+
+class GGHHH(models.Model):
+    "Generated Model"
+    hhjhjh = models.BigIntegerField()
