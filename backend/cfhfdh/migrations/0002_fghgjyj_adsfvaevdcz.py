55c8dca65d39a9cada107083ea5a7d3614fc0eee
Crowdbotics <team@crowdbotics.com>
2020-12-04 13:47:48 +0000

    Update models

--- backend/cfhfdh/migrations/0002_fghgjyj_adsfvaevdcz.py
+++ backend/cfhfdh/migrations/0002_fghgjyj_adsfvaevdcz.py
@@ -7,14 +7,20 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('homejhgjhgjg', '0001_initial'),
-        ('cfhfdh', '0001_initial'),
+        ("homejhgjhgjg", "0001_initial"),
+        ("cfhfdh", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='fghgjyj',
-            name='adsfvaevdcz',
-            field=models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='fghgjyj_adsfvaevdcz', to='homejhgjhgjg.JHGjfhg'),
+            model_name="fghgjyj",
+            name="adsfvaevdcz",
+            field=models.OneToOneField(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="fghgjyj_adsfvaevdcz",
+                to="homejhgjhgjg.JHGjfhg",
+            ),
         ),
     ]
--- backend/cfhfdh/models.py
+++ backend/cfhfdh/models.py
@@ -7,10 +7,17 @@ class Fghgjyj(models.Model):
     ftfttr = models.BigIntegerField()
     adsfvaevdcz = models.OneToOneField(
         "homejhgjhgjg.JHGjfhg",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="fghgjyj_adsfvaevdcz",
+    )
+    dcvae = models.OneToOneField(
+        "homejhgjhgjg.JHGjfhg",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name="fghgjyj_adsfvaevdcz",
+        related_name="fghgjyj_dcvae",
     )
 
 
