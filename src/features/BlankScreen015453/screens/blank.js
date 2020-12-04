521feae157ac164f3ba7e1fa31706b54d4d8586f
Crowdbotics <team@crowdbotics.com>
2020-12-04 05:27:48 +0000

    Update components

--- src/features/BlankScreen015453/screens/blank.js
+++ src/features/BlankScreen015453/screens/blank.js
@@ -23,14 +23,52 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { TextInput_4: "" }
 
   render = () => (
     <View>
       <Text>Sample text content</Text>
       <Text>Sample text content</Text>
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_4}
+        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
+      />
+      <View style={styles.View_5}>
+        <View />
+        <View />
+        <View />
+        <View />
+        <View />
+      </View>
+      <View style={styles.View_8}>
+        <View />
+        <View />
+      </View>
+      <View style={styles.View_11}>
+        <View />
+        <View />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({ View_1: {}, Text_2: {}, Text_3: {} })
+const styles = StyleSheet.create({
+  View_1: {},
+  Text_2: {},
+  Text_3: {},
+  TextInput_4: {},
+  View_5: { flexDirection: "row" },
+  View_6: {},
+  View_7: {},
+  View_14: {},
+  View_15: {},
+  View_16: {},
+  View_8: { flexDirection: "row" },
+  View_9: {},
+  View_10: {},
+  View_11: { flexDirection: "row" },
+  View_12: {},
+  View_13: {}
+})
